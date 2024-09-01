9379fd3e9e3c0a0b7d22099e260fddbe0e64644f
TinyPandas <chris.barwick16@gmail.com>
2024-09-01 03:10:34 -0700

    Integrate Discord/Auth0

--- amplify/auth/resource.ts
+++ amplify/auth/resource.ts
@@ -1,11 +1,31 @@
-import { defineAuth } from '@aws-amplify/backend';
+import { defineAuth, secret } from '@aws-amplify/backend';
 
-/**
- * Define and configure your auth resource
- * @see https://docs.amplify.aws/gen2/build-a-backend/auth
- */
 export const auth = defineAuth({
   loginWith: {
     email: true,
-  },
+    externalProviders: {
+      oidc: [
+        {
+          name: "auth0",
+          /**
+           * Since in Amplify, the TypeScript definition of 
+           * clientId and clientSecret is BackendSecret,
+           * we need to store the values in Amplify's secret manager.
+           */
+          clientId: secret("authClient"),
+          clientSecret: secret("authSecret"),
+          issuerUrl: "https://dev-wvc4ao3ybzbt1ioa.us.auth0.com",
+          scopes: ["openid", "profile", "email", "name"],
+        },
+      ],
+      logoutUrls: [
+        "http://localhost:5173",
+        "https://pandabot.gg",
+      ],
+      callbackUrls: [
+        "http://localhost:5173",
+        "https://pandabot.gg",
+      ],
+    }
+  }
 });
