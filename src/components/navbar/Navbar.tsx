443b7097a9a3977fcc7d3f0134cc1a14f6edad36
TinyPandas <chris.barwick16@gmail.com>
2024-09-01 21:42:50 -0700

    Fix FontAwesomeIcon imports

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faUser } from '@fortawesome/fontawesome-free-solid'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser } from '@fortawesome/free-regular-svg-icons';
 import './Navbar.css';
 
 const Navbar = () => {
